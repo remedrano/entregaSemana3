4972a3fd45c949532691802bb5cced11df7a82cc
Rafael Enrique Medrano Torres <crmedran1@idu.gov.co>
2018-09-09 15:10:22 -0500

    Agregar resultados y comparaciones

---
+++ losestudiantes.spec.js
@@ -0,0 +1,25 @@
+var assert = require('assert');
+describe('los estudiantes login', function() {
+    it('should visit los estudiantes and failed at log in', function () {
+		
+		
+        browser.url('https://losestudiantes.co');
+        browser.click('button=Cerrar');				
+        browser.waitForVisible('button=Ingresar', 5000);
+        browser.click('button=Ingresar');
+		
+		//Buscar profesor
+		browser.waitForVisible('.Select-control', 5000);
+        var selector = browser.element('.Select-control');
+	
+        selector.click();
+		selector.keys('Dario Correal');   
+
+		//Mostrar pagina principal profesor
+		browser.waitForVisible('.Select-control', 5000);
+        var selectorPrincipal = browser.element('.Select-menu-outer');
+		selectorPrincipal.selectByVisibleText('Dario Correal');
+        selectorPrincipal.click();
+		
+    });
+});
